6cd5607432089840d990602d5b421e856652539d
cy <cy@example.com>
2025-12-04 11:53:19 +0800

    implemented guest homepage, article page, login page, admin page and edit/new article page

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # personal_blog
 This repository is a implementation of roadmap.sh backend project (https://roadmap.sh/projects/personal-blog).
+
+This frontend and backend is implemented by html(plus ejs for templating) and node.js respectively.
+framework bootstrap is used.
+
+please follow the instructions to run:
+1. download and unzip the code
+2. open terminal and go the project folder
+3. type `npm install` in terminal to install the neccessary node package
+4. type `node index.js` to run the backend. [do not run with `nodemon`. `nodemon` will re-start after writing or deleting file (create a new article, save modifed an article and delete an article). It needs to log in again when `nodemon` will re-start.]
+5. Port `${PORT}` is opened. Use web browser to open `http://localhost:${PORT}/home` to view the articles as a guest.
+6. To create, edit or delete articles, go to `http://localhost:${PORT}/login` to login as admin (username and password is stored in index.js for simplicity).
+
+home page (guest)
+![display home page](./screenshot/home_page.png)
+
+article content
+![display article content](./screenshot/article_page.png)
+
+admin page
+![display the admin page](./screenshot/admin_page.png)
+
+edit article
+![display the editing page](./screenshot/edit_page.png)
+
+
+This project use `express-session` to store user login status. Any access to `/admin`, `/edit/x` and `/new` page as guest will be redirected to `/login` page.
+
+The newest article (published date) will be listed at the top in `/home` and `/admin` page. The order of article will be sorted before display when a new article has been published or an existing article has been editted each time.
---
+++ index.js
@@ -0,0 +1,329 @@
+import express from "express";
+import bodyParser from "body-parser";
+import fs from "fs";
+import session from "express-session";
+
+const app = express();
+const PORT = 4000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(express.static("public"));
+
+
+// Configure sessions
+app.use(session({
+  secret: 'secret-key',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: false, maxAge: 24 * 60 * 60 * 1000 } // 24 hours
+}));
+
+
+// The User account information is stored here for simplicity of demonstration. It should be stored in a database.
+const users = [
+  { id: 1, username: 'user1', password: 'password1' }
+];
+
+
+let articleSequenceNumber = 1;
+
+const ARTICLE_DIRECTORY = "./articles";
+const SEQUENCE_FILE = "sequenceNumber.json";
+
+let articlesArray = new Array();
+let isArticlesSorted = false;
+/*
+let articlesArray = [
+  {title:"hello", date:new Date(2025, 10, 13, 3, 24, 0), content:"This is 1st test article"},
+  {title:"world", date:new Date(2025, 10, 17, 10, 2, 0), content:"This is 2nd test article"},
+  {title:"hand", date:new Date(2025, 11, 1, 1, 40, 0), content:"This is 3rd test article"},
+  {title:"make", date:new Date(2025, 11, 3, 5, 17, 0), content:"This is 4th test article"},
+  {title:"figure", date:new Date(2026, 2, 3, 5, 17, 0), content:"This is 5th test article"},
+];
+*/
+
+function readSequenceFile(){
+  const fileName = `./${SEQUENCE_FILE}`;
+  fs.readFile(fileName, 'utf8', (err, data)=>{
+    if(!err)
+      articleSequenceNumber = JSON.parse(data);
+    else
+      console.log("fail to read sequence file", err);
+  });
+}
+
+function writeSequenceFile(){
+  const fileName = `./${SEQUENCE_FILE}`;
+  fs.writeFile(fileName, JSON.stringify(articleSequenceNumber, null, 4), 'utf8', (err)=>{
+    if(err)
+      console.error("fail to write sequenceNumber file", err);
+  });
+}
+
+function incrementSequenceNumber(){
+  articleSequenceNumber += 1;
+  writeSequenceFile();
+}
+
+
+function writeArticleIntoFile(article) {
+
+  // store article in JSON format
+  const fileName = `${ARTICLE_DIRECTORY}/${articleSequenceNumber}.json`;
+  fs.writeFile(fileName, JSON.stringify(article, null, 4), 'utf8', (err)=>{
+    if(err)
+      console.error("fail to write article file", err);
+  });
+
+  incrementSequenceNumber();
+}
+
+//overwrite the content of the original file.
+function updateArticleFile(article){
+    const fileName = `${ARTICLE_DIRECTORY}/${article.id}.json`;
+    fs.writeFile(fileName, JSON.stringify(article, null, 4), 'utf8', (err)=>{
+      if(err)
+        console.error("fail to write article file", err);
+    });
+}
+
+
+function readArticleFiles(){
+  fs.readdir(ARTICLE_DIRECTORY, {encoding:'utf8'}, (err, files)=>{
+      if(err){
+        console.error("fail to read articile directory", err);
+      }else{
+         files.forEach((file)=>{
+          //console.log(`${ARTICLE_DIRECTORY}/${file}`);
+          fs.readFile(`${ARTICLE_DIRECTORY}/${file}`, 'utf8', (err, data)=>{
+            //to convert the string of date into Date object during parsing process
+            articlesArray.push(JSON.parse(data, parseDate));  
+            //console.log(data);
+          });
+        });
+      }
+  });
+
+}
+
+
+function deleteArticleFile(articleId){
+
+  fs.rm(`${ARTICLE_DIRECTORY}/${articleId}.json`, (err)=>{
+    if(err){
+      console.log("fail to delete file with id " + articleId);
+    }
+      
+  });
+}
+
+/**
+ * It is reviver function for parse JSON to return a Date object.
+ * @param {*} key the key of JSON object
+ * @param {*} value the value produced by parsing the JSON key
+ * @returns if the key is "date", it turn Date object. Otherwise, it return original value
+ */
+function parseDate(key, value) {
+  if (key === "date") {
+    return new Date(value);
+  }
+  return value;
+}
+    
+
+/**
+ * return Date object
+ * @param {*} inputDate a string in date format "yyyy-mm-dd", which received from HTML input tag with type="date"
+ */
+function createDateFromDateInput(inputDate){
+    const [year, month, day] = inputDate.split("-");
+    const date = new Date(year, month - 1, day, 0, 0, 0);
+    return date;
+}
+
+function convertToStringFormatForDateInputTag(date){
+    let dateString = date.toLocaleDateString("en-US", {year: "numeric", month: "2-digit", day: "2-digit",}); //String is mm/dd/yyyy
+      //console.log("before replacement, date is " + dateString);
+    const [month, day, year] = dateString.split('/');
+    dateString = year + "-" + month + "-" + day;
+    return dateString;
+}
+
+readSequenceFile();
+readArticleFiles();
+
+
+// Route to render the edit page
+// This page can only been accessed after login
+app.post('/edit/:articleID', (req, res) =>{
+
+  if(!req.session.user){
+    return res.redirect("/login");
+  }
+
+  const id = req.params.articleID;
+  //check if articleID is digit number
+  if(id.match(/\d+/)){ 
+    if(id < articleSequenceNumber && id > 0){
+      const articleElement = articlesArray.find((article)=> article.id == id);
+      const dateString = convertToStringFormatForDateInputTag(articleElement.date);
+      res.render("edit_article.ejs", {article:articleElement, date:dateString});
+    }else
+      res.send('404');
+  }else{
+    res.send('404');
+  }
+  
+});
+
+
+// Route to create new article, update or delete an existing article. Then redirect to /admin page
+app.post("/modified", (req, res)=>{
+  
+  if(!req.session.user){
+      return res.redirect("/login");
+  }
+
+  //console.log("post /admin with action " + req.body.action);
+  
+  //delete an article
+  if(req.body.action === "Delete"){
+    const index = articlesArray.findIndex((article) => article.id == req.body.deleteArticleId);
+    deleteArticleFile(req.body.deleteArticleId);
+    articlesArray.splice(index, 1);
+    console.log(`deleted article ${req.body.deleteArticleId}!`);
+
+  //publish a new article
+  }else if(req.body.action === "Publish"){
+    console.log("publish new article");
+    const dateObj = createDateFromDateInput(req.body.date);
+    const newArticle = {id: articleSequenceNumber,title:req.body.title, date:dateObj, content:req.body.content};
+    writeArticleIntoFile(newArticle);
+    articlesArray.splice(0, 0, newArticle);
+    isArticlesSorted = false;
+
+  //update the article
+  }else if(req.body.action === "Update"){
+    const id = req.body.articleId;
+    if(id < articleSequenceNumber && id > 0){
+      const articleElement = articlesArray.find((article)=> article.id == id);
+      const dateObj = createDateFromDateInput(req.body.date);
+      articleElement.title = req.body.title;
+      articleElement.date = dateObj;
+      articleElement.content = req.body.content;
+      updateArticleFile(articleElement);
+      isArticlesSorted = false;
+    }
+  }
+    
+  res.redirect("/admin");
+
+});
+
+
+// Route to render the new article page
+// This page can only been accessed after login
+app.get("/new", (req, res) =>{
+
+    //go to login page when user is guest
+  if(!req.session.user){
+    return res.redirect("/login");
+  }
+ 
+  const date = new Date();
+  const dateString = convertToStringFormatForDateInputTag(date);
+  res.render("edit_article.ejs", {date:dateString});
+});
+
+// Route to render admin page
+// This page can only been accessed after login
+app.get("/admin", (req,res)=>{
+
+  
+  //go to login page when user is guest
+  if(!req.session.user){
+    return res.redirect("/login");
+  }
+ 
+  // sort the article in descending order of date
+  if(!isArticlesSorted){
+    articlesArray.sort((a, b)=> b.date - a.date);
+    isArticlesSorted = true;
+  }
+  res.render("admin.ejs", {articles:articlesArray});
+});
+
+
+app.get("/logout", (req, res)=>{
+    req.session.destroy((err) => {
+    if (err) {
+      return res.status(500).json({ message: 'Logout failed' });
+    }
+      res.redirect("/home");
+    });
+});
+
+
+
+app.post("/login", (req, res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const loginUser = users.find((user) => user.username === username && user.password === password);
+    if(!loginUser){
+      return res.render("login.ejs", {incorrect:true});
+    }
+
+    // Store user information in session (excluding password)
+    req.session.user = {
+      id: loginUser.id,
+      username: loginUser.username
+    };
+
+    res.redirect("/admin");
+
+});
+
+//Route to render login page
+app.get("/login", (req, res)=>{
+  res.render("login.ejs");
+});
+
+
+// Route to render the aritcle detail page
+app.get('/article/:articleID', (req, res) =>{
+  const id = req.params.articleID;
+   //check if articleID is digit number
+  if(id.match(/\d+/)){ 
+    if(id <= articleSequenceNumber && id > 0){     
+      const articleElement = articlesArray.find((article)=> article.id == id);
+      res.render("article.ejs", {article:articleElement});
+    }else{
+      res.send('404');
+    }
+  }else{
+    res.send('404');
+  }
+  
+});
+
+// Route to render the Guset home page
+app.get("/home", (req, res) =>{
+  
+  // sort the article in descending order of date
+  if(!isArticlesSorted){
+    articlesArray.sort((a, b)=> b.date - a.date);
+    isArticlesSorted = true;
+  }
+  res.render("guest_home.ejs", {articles:articlesArray});
+});
+
+app.get("/", (req, res) =>{
+  res.redirect("/home");
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Listening to port ${PORT}`);
+});
+
